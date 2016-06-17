0e2f349908cac6a785d51e3f03bc2bdc84670f73
Ashleigh Adams <ashleighadams318@hotmail.co.uk>
2016-06-17 12:57:25 +0100

    added library class

--- specs/playlist_spec.rb
+++
@@ -1,9 +0,0 @@
-require('minitest/autorun')
-require_relative('../playlist')
-
-class TestPlaylist < Minitest::Test
-
-  def setup
-    @song_1 = Song.new("Girls just want to have fun", "Cyndi Lauper", genre = ["Pop", "Dance", "New wave"], "70's")
-  end
-end
